4d029f79a96fad1ad9fe23ec30ee4c89d1172d89
H-Cheng-C <89052553+H-Cheng-C@users.noreply.github.com>
2021-09-11 12:20:37 +0800

    Delete qinchengcode/qingcheng_parent directory

--- qinchengcode/qingcheng_parent/qingcheng_interface/src/main/java/com/qingcheng/service/goods/SkuSearchService.java
+++
@@ -1,9 +0,0 @@
-package com.qingcheng.service.goods;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface SkuSearchService {
-
-    public Map search(Map<String,String> searchMap) throws IOException;
-}
--- qinchengcode/qingcheng_parent/qingcheng_service_seckill/src/main/java/com/qingcheng/dao/SeckillGoodsMapper.java
+++
@@ -1,8 +0,0 @@
-package com.qingcheng.dao;
-
-import com.qingcheng.pojo.seckill.SeckillGoods;
-import tk.mybatis.mapper.common.Mapper;
-
-
-public interface SeckillGoodsMapper extends Mapper<SeckillGoods> {
-}
